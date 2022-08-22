f774103003ecde65f2ec82cba8f61730a9cf94eb
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-08-22 16:55:50 +0200

    implemented the first actions in the offers controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,10 +1,20 @@
 class OffersController < ApplicationController
   def index
+    @offers = Offer.all
   end
 
   def new
+    @offer = Offer.new
   end
 
   def create
+    @offer = Offer.new(offer_params)
+    @offer.save
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :date, :hourly_rate, :location, :picture_url)
   end
 end
