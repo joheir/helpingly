08bcb6208f88ae4ca60c9657b43a8545a13d87a0
maartje gubbels <m.gubbels96@gmail.com>
2022-08-23 09:48:14 +0200

    added login

--- db/migrate/20220822151822_devise_create_users.rb
+++ db/migrate/20220822151822_devise_create_users.rb
@@ -2,17 +2,17 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
+    change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      #t.string :email,              null: false, default: ""
+      t.column :encrypted_password, :string, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      t.column   :reset_password_token, :string
+      t.column :reset_password_sent_at, :datetime
 
       ## Rememberable
-      t.datetime :remember_created_at
+      t.column :remember_created_at, :datetime
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -33,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+     # t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
