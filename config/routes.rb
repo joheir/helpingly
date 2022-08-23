e60b92571f5b2c4eae377e79b06d02e1824f3695
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-08-23 15:00:15 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "/owner-profile", to: "pages#owner_profile"
+  # get "/owner-profile", to: "pages#owner_profile"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   resources :offers, only: %i[index new create]
+  resources :user, only: %i[show]
   # root "articles#index"
 end
