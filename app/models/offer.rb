5a6229ccf18d8d243c541e8e62d7ed039a4a8113
Yami Pedroso-AI <yamirovinci@gmail.com>
2022-08-24 13:38:30 +0200

    Working on it

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,5 +2,5 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_many :appointments
   has_one_attached :photo
-  validates :title, :description, :date, :hourly_rate, :location, presence: true
+  validates :title, :description, :date, :hourly_rate, :location, :photo, presence: true
 end
