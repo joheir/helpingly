4e76d2f3595c82d946c18dfc67a0965964011083
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-08-25 13:49:36 +0200

    added migration for the offer table and modified the form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_092721) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_113213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_092721) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "category"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
