35746d64aeb4ddc72ec8b076ffa792942465a628
maartje gubbels <m.gubbels96@gmail.com>
2022-08-25 15:52:37 +0200

    appointment added

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,17 @@
 class AppointmentsController < ApplicationController
   def new
     @appointment = Appointment.new
+    @offer = Offer.find(params[:offer_id])
   end
 
   def create
+    @offer = Offer.find(params[:offer_id])
     @appointment = Appointment.new(appointment_params)
+    @appointment.user = current_user
+    @appointment.offer = @offer
 
     if @appointment.save
-      redirect_to current_user, notice: "Appointment was successfully booked."
+      redirect_to user_path(current_user), notice: "Appointment was successfully booked."
     else
       render :new, status: :unprocessable_entity
     end
